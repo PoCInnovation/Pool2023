6bdf2bc190def478cb414df97c434d564081edab
Ylan <ylan.garnier@epitech.eu>
2022-12-09 16:08:36 +0100

    [ADD] easy challs 1, 3 and half of chall 4

---
+++ Security/Day-web/Easy/chall-01-Lenna/app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__, template_folder='template', static_folder='static')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/index.html~')
+def backup():
+    return send_file("template/index.html~", as_attachment=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 4001))
+    app.run(debug=True, host='localhost', port=port)
