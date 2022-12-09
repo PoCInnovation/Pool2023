34ce6f6974bd0e1d8ee60bd59522af5a3a0d9ffc
Ylan <ylan.garnier@epitech.eu>
2022-12-09 16:08:36 +0100

    [ADD] easy challs 1, 3 and half of chall 4

---
+++ Security/Day-web/Easy/chall-03-Ismy/app.py
@@ -0,0 +1,25 @@
+import os
+import subprocess as sp
+
+from flask import Flask, make_response, render_template, request
+
+app = Flask(__name__, template_folder='template', static_folder='static')
+
+
+@app.route('/', methods =['GET'])
+def home():
+    login = request.headers.get('Login-Status')
+    if (login == 'Admin' or login == 'Hacker'):
+        return render_template('flag.html', flag=os.getenv('FLAG'))
+    out = sp.run(["php", "template/index.php"], stdout=sp.PIPE)
+    return (out.stdout, {'Login-Status': 'Simple User'})
+
+@app.route('/robots.txt', methods=['GET'])
+def robots():
+    response = make_response(open('static/robots.txt').read())
+    response.headers["Content-type"] = "text/plain"
+    return response
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 4003))
+    app.run(debug=True, host='0.0.0.0', port=port)
