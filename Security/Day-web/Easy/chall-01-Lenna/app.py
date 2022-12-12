eaa9a6eee851e070b2377d5a38cd85b262f36541
Lenny Vongphouthone <lenny.vongphouthone@epitech.eu>
2022-12-12 21:44:11 +0100

    [edit] : change localhost by 0.0.0.0

--- Security/Day-web/Easy/chall-01-Lenna/app.py
+++ Security/Day-web/Easy/chall-01-Lenna/app.py
@@ -16,4 +16,4 @@ def backup():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 4001))
-    app.run(debug=True, host='localhost', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
