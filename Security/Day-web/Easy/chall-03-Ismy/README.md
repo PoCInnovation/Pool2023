d660c87509151e2c667081b980cd7225a8fab8f7
Lenny Vongphouthone <lenny.vongphouthone@epitech.eu>
2023-01-28 20:48:23 +0100

    Pathwarisation

---
+++ Security/Day-web/Easy/chall-03-Ismy/README.md
@@ -0,0 +1,4 @@
+# challenge-template
+A Pathwar Challenge template to get quickly started
+
+Quick Start: https://github.com/pathwar/pathwar/wiki/Challenge-Maintainers
--- Security/Day-web/Easy/chall-03-Ismy/front/app.py
+++ Security/Day-web/Easy/chall-03-Ismy/front/app.py
@@ -21,5 +21,5 @@ def robots():
     return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 4003))
+    port = int(os.environ.get('PORT', 80))
     app.run(debug=True, host='0.0.0.0', port=port)
