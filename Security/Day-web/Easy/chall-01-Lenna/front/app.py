e8f83dd561d65cc9632e3ecd88f696634852c76b
Lenny Vongphouthone <lenny.vongphouthone@epitech.eu>
2023-01-28 20:48:23 +0100

    Pathwarisation

--- Security/Day-web/Easy/chall-01-Lenna/front/app.py
+++ Security/Day-web/Easy/chall-01-Lenna/front/app.py
@@ -15,5 +15,5 @@ def backup():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 4001))
+    port = int(os.environ.get('PORT', 80))
     app.run(debug=True, host='0.0.0.0', port=port)
