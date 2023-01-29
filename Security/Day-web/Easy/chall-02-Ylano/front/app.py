22c0619079109fcb05779f37c23c85d6f400bdc4
Antony JIN <91880456+Antonyjin@users.noreply.github.com>
2023-01-29 01:37:15 +0100

    Update app.py

--- Security/Day-web/Easy/chall-02-Ylano/front/app.py
+++ Security/Day-web/Easy/chall-02-Ylano/front/app.py
@@ -10,5 +10,5 @@ def home():
     return render_template('index.php')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 4002))
+    port = int(os.environ.get('PORT', 80))
     app.run(debug=True, host='0.0.0.0', port=port)
