17bdfd3b1e1a1b3b1e1613705d42cacdbf6b1d8b
Antonyjin <antonyjin@hotmail.fr>
2023-01-29 01:49:13 +0100

    [ADD] chall4 for pool exploit filter in php with include

--- Security/Day-web/Easy/chall-02-Ylano/front/app.py
+++ Security/Day-web/Easy/chall-02-Ylano/front/app.py
@@ -1,4 +1,5 @@
 import os
+import subprocess as sp
 
 from flask import Flask, render_template, send_file
 
@@ -7,7 +8,8 @@ app = Flask(__name__, template_folder='template', static_folder='static')
 
 @app.route('/')
 def home():
-    return render_template('index.php')
+    out = sp.run(["php", "template/index.php"], stdout=sp.PIPE)
+    return out.stdout
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 80))
