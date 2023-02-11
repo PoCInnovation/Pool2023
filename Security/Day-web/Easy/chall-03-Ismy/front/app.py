9f4a1b0e39c6e2ce1e4e8ca4563bd83824bf6831
Ylan Garnier <ylan.garnier@epitech.eu>
2023-02-11 19:03:38 +0100

    [ADD] fix chall port

--- Security/Day-web/Easy/chall-03-Ismy/front/app.py
+++ Security/Day-web/Easy/chall-03-Ismy/front/app.py
@@ -25,5 +25,5 @@ def robots():
     return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 80))
     app.run(debug=True, host='0.0.0.0', port=port)
